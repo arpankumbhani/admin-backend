d256ed71295bd0d5e49e2cdff87e154bf9d7071f
arpankumbhani <arpankumbhani@gmail.com>
2023-11-26 14:09:35 +0530

    code update

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ app.use(cors({ origin: "*" }));
 //load config from env file
 
 require("dotenv").config();
-const PORT = process.env.PORT || 6000;
-
 //middleware to parse json request body
 app.use(express.json());
 
@@ -15,15 +13,28 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/user"));
 
 //start server
-app.listen(PORT, () => {
-  console.log(`Server started successfully at ${PORT}`);
+
+app.get("/", (req, res) => {
+  res.send({
+    status: "ok",
+  });
+});
+
+app.get("/ping", (req, res) => {
+  res.send("pong 🏓");
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, (err, res) => {
+  if (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  } else {
+    console.log("[INFO] Server Running on port:", port);
+  }
 });
 
 //connect to database
 const dbConnect = require("./config/database");
 dbConnect();
-
-//default routes
-app.get("/", (req, res) => {
-  res.send(`<h1> This is chalange </h1>`);
-});
